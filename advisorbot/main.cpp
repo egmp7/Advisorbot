a5c77ee7b6e7bac3b5e76a5151a933f2eaadb13b
Erick Gonzalez <deweyerick@gmail.com>
2022-12-15 11:32:05 -0600

    Initial Commit

---
+++ advisorbot/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  advisorbot
+//
+//  Created by Erick Gonzalez on 15/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
